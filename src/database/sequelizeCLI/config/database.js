1de68c452223a05085197a8c25af3686e820181c
TriPCViviix <trihuynh@viviix.com>
2025-05-23 12:37:33 +0700

    WIP2

--- src/database/sequelizeCLI/config/database.js
+++ src/database/sequelizeCLI/config/database.js
@@ -4,7 +4,7 @@ let config = {
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  host: process.env.DB_HOST,
+  host: process.env.DB_HOST || 'db',
   dialect: process.env.DB_DIALECT,
   port: parseInt(process.env.DB_PORT) || 3306,
   logging: false,

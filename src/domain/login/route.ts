c426231fdc844a1b023b267dd09264027e5db0fb
TriPCViviix <trihuynh@viviix.com>
2026-01-09 10:25:10 +0700

    feat: add OTP validation middleware import to login route

--- src/domain/login/route.ts
+++ src/domain/login/route.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import { signin } from "./controller";
 import { register } from "./controller";
+import { validateOTP } from "../../middleware/optValidation.middleware";
 
 const router = Router();
 
 router.post("/register", register);
 router.post("/signin", signin);
+// router.post("/validate-otp", validateOTP);
 
 export default router;
